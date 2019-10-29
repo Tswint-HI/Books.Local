981cc7bd2546caaf7a70b51e98bae60d380262db
Tswint <tswint@horizontalintegration.com>
2019-10-29 09:54:41 -0500

    correctly configured orm proj

---
+++ src/Feature/Hero/code/HeroGlassLoader.cs
@@ -0,0 +1,14 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Pipelines.GetGlassLoaders;
+
+namespace Books.Feature.Hero
+{
+    public class HeroGlassLoader : GetGlassLoadersProcessor
+    {
+        public override void Process(GetGlassLoadersPipelineArgs args)
+        {
+            SitecoreAttributeConfigurationLoader loader = new SitecoreAttributeConfigurationLoader("Books.Feature.Hero");
+            args.Loaders.Add(loader);
+        }
+    }
+}
