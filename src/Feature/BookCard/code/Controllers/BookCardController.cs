32321a93f2b0d49d0318b7095e67a31413516357
Tswint <tswint@horizontalintegration.com>
2019-11-11 18:18:24 -0600

    Initial BookCard component added

---
+++ src/Feature/BookCard/code/Controllers/BookCardController.cs
@@ -0,0 +1,22 @@
+﻿using Glass.Mapper.Sc.Web.Mvc;
+using System;
+using System.Web.Mvc;
+
+namespace Books.Feature.BookCard.Controllers
+{
+    public class BookCardController : Controller
+    {
+        private readonly IMvcContext _context;
+
+        public BookCardController(IMvcContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public ActionResult ActionResult()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ src/Feature/BookCard/code/Pipelines/BookCardGlassLoader.cs
@@ -0,0 +1,14 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Pipelines.GetGlassLoaders;
+
+namespace Books.Feature.BookCard
+{
+    public class BookCardGlassLoader : GetGlassLoadersProcessor
+    {
+        public override void Process(GetGlassLoadersPipelineArgs args)
+        {
+            SitecoreAttributeConfigurationLoader loader = new SitecoreAttributeConfigurationLoader("Books.Feature.Carousel");
+            args.Loaders.Add(loader);
+        }
+    }
+}
