fec372a3638708cf45a9afb542549ab1cdbc0df1
Tswint <tswint@horizontalintegration.com>
2019-12-09 11:09:27 -0600

    Add search proj

---
+++ src/Feature/Search/code/App_Start/RegisterCustomRoute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.UI.WebControls;
+using Sitecore.Pipelines;
+
+namespace Books.Feature.Search.App_Start
+{
+    public class RegisterCustomRoute
+    {
+        public virtual void Process(PipelineArgs args)
+        {
+            Register();
+        }
+        public static void Register()
+        {
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            RouteTable.Routes.MapRoute("CustomRoute", "api/testing/{controller}/{action}");
+        }
+    }
+}
---
+++ src/Feature/Search/code/Pipelines/SearchGlassLoader.cs
@@ -0,0 +1,14 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Pipelines.GetGlassLoaders;
+
+namespace Books.Feature.Search.Pipelines
+{
+    public class SearchGlassLoader : GetGlassLoadersProcessor
+    {
+        public override void Process(GetGlassLoadersPipelineArgs args)
+        {
+            SitecoreAttributeConfigurationLoader loader = new SitecoreAttributeConfigurationLoader("Books.Feature.Search");
+            args.Loaders.Add(loader);
+        }
+    }
+}
